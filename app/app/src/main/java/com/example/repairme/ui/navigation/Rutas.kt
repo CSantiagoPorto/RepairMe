0c161110c98085d4348b93b423557b30629adcbe
CSantiagoPorto <148074839+CSantiagoPorto@users.noreply.github.com>
2026-02-13 10:57:33 +0100

    Funciona el registro y la navegación desde login

--- app/app/src/main/java/com/example/repairme/ui/navigation/Rutas.kt
+++ app/app/src/main/java/com/example/repairme/ui/navigation/Rutas.kt
@@ -3,7 +3,7 @@ package com.example.repairme.ui.navigation
 sealed class Rutas (val ruta:String){//Equivale a un enum
     data object  LOGIN: Rutas("login")
     data object  REGISTRO: Rutas("registro")
-    data object  USERSCREEN: Rutas("pantalla user")
+    data object  USERSCREEN: Rutas("pantallaUser")
 
     //Aquí le estoy diciendo que Login pues es un tipo de ruta
     //con el data object le hacemos un singleton a la instancia de la pantalla y tenemos
