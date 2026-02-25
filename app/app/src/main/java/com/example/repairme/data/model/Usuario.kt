adc94902effa1005c42cfe4c879c3c18868fa69c
CSantiagoPorto <148074839+CSantiagoPorto@users.noreply.github.com>
2026-02-25 09:33:34 +0100

    Biblio

--- app/app/src/main/java/com/example/repairme/data/model/Usuario.kt
+++ app/app/src/main/java/com/example/repairme/data/model/Usuario.kt
@@ -3,7 +3,7 @@ package com.example.repairme.data.model
 import com.google.firebase.database.Exclude
 
 data class Usuario (
-   @get:Exclude val id: String = "",//Necesito excluirlo como documento en la bbdd pero lo necesito luego para poder filtrar por id y recuperar luego una luista de users
+   @get:Exclude val id: String = "",//Necesito excluirlo como documento en la bbdd pero lo necesito luego para poder filtrar por id y recuperar luego una luista de users.
     val name: String = "",
     val email: String = "",
     val phone: String = "",
