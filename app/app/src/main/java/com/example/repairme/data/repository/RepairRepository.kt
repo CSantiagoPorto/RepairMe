38b299f44ce50e6473a0eb83b3b102a130456747
AlexCanadas <147659337+AlexCanadas@users.noreply.github.com>
2026-03-05 03:23:19 +0100

    Add: CRUD equipos y averias

--- app/app/src/main/java/com/example/repairme/data/repository/RepairRepository.kt
+++ app/app/src/main/java/com/example/repairme/data/repository/RepairRepository.kt
@@ -2,14 +2,11 @@ package com.example.repairme.data.repository
 
 import com.example.repairme.data.model.Averia
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.FirebaseDatabase
 
-class RepairRepository {
+class RepairRepository : OperationsTemplateRepository() {
+
     private val auth = FirebaseAuth.getInstance()
-    private val database = FirebaseDatabase.getInstance(
-        "https://repairme-956fd-default-rtdb.europe-west1.firebasedatabase.app"
-    )
-    val averiaRef= database.getReference("repairs")
+    private val NODE = "repairs"
 
     fun abrirAveria(
         averia: Averia,
@@ -24,12 +21,13 @@ class RepairRepository {
             return
 
         }
-        val averiaRef= database.getReference("repairId")
-        val averiaUid= averiaRef.push().key
+
+        val averiaRef= ref("repairId")
+        val averiaUid= newId("repairId")
 
         //Push me crea el ID y key me devuelve el id
 
-        if(averiaUid==null){
+        if(averiaUid.isBlank()){
             fallo("Se produjo un error al creae el id del dispositivo")
             return
 
@@ -40,12 +38,44 @@ class RepairRepository {
             userId= userId
         )
 
-
+        // !!! CREO QUE SE GUARDA UN STRING Y NO EL OBJETO !!!
         averiaRef.child(averiaUid).setValue("repair")
             .addOnSuccessListener { exito() }
-            .addOnFailureListener{ e-> fallo("Error al guardar: ${e.message}")}
+            .addOnFailureListener{ e-> fallo("Error al guardar: ${e.message}") }
     }
 
+    //Editar avería completa
+    fun editarAveria(
+        averiaEditada:Averia,
+        exito:()->Unit,
+        fallo:(String)->Unit
+    ){
+        if(averiaEditada.id.isBlank()){
+            fallo("La avería no tiene id")
+            return
+        }
 
+        setValue("$NODE/${averiaEditada.id}", averiaEditada,
+            ok = { exito() },
+            error = { msg-> fallo(msg) }
+        )
+    }
+
+    //Eliminar avería
+    fun eliminarAveria(
+        averiaId:String,
+        exito:()->Unit,
+        fallo:(String)->Unit
+    ){
+        if(averiaId.isBlank()){
+            fallo("La avería no tiene id")
+            return
+        }
+
+        delete("$NODE/$averiaId",
+            ok = { exito() },
+            error = { msg-> fallo(msg) }
+        )
+    }
 
 }
