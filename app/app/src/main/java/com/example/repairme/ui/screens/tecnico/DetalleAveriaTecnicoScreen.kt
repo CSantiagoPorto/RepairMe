a19ff78811a515787416ffade07b03fcbea5c043
CSantiagoPorto <148074839+CSantiagoPorto@users.noreply.github.com>
2026-05-08 12:46:45 +0200

    Modificada la llamada para que obtenga el id del técnico

--- app/app/src/main/java/com/example/repairme/ui/screens/tecnico/DetalleAveriaTecnicoScreen.kt
+++ app/app/src/main/java/com/example/repairme/ui/screens/tecnico/DetalleAveriaTecnicoScreen.kt
@@ -210,9 +210,6 @@ fun DetalleAveriaTecnicoScreen(
                                         fallo = {}
 
                                     )
-
-
-
                                 }
                             }) {
                                 Text("Enviar presupuesto")
